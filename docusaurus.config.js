505fafcc080ef3655029cb1e3cff99a577b339c5
sophia <sjudish@uw.edu>
2023-07-10 12:27:42 -0700

    search bar update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -53,12 +53,12 @@ const config = {
     ({
     algolia: {
         // The application ID provided by Algolia
-        appId: 'YOUR_APP_ID',
+        appId: 'FAZ4EOEIYS',
 
         // Public API key: it is safe to commit it
-        apiKey: 'YOUR_SEARCH_API_KEY',
+        apiKey: 'e6346fbe3575ab0a53d7a018ed37923a',
 
-        indexName: 'YOUR_INDEX_NAME',
+        indexName: 'kanoa-software',
 
         // Optional: see doc section below
         contextualSearch: true,
