8d07c79fcb30e3da68aedfbf6c720b5dcb6bc327
sophia <sjudish@uw.edu>
2023-11-19 22:25:08 -0800

    sidebar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,6 +41,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          breadcrumbs: false,
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
